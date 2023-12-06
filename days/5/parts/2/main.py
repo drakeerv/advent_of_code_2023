89c099735ae694b4dbfef81af5b6fb14703c0fe2
drakeerv <drakeerv@outlook.com>
2023-12-05 20:28:04 -0500

    Finish day 5

--- days/5/parts/2/main.py
+++ days/5/parts/2/main.py
@@ -14,7 +14,6 @@ In the above example, the lowest location number can be obtained from seed numbe
 Consider all of the initial seed numbers listed in the ranges on the first line of the almanac. What is the lowest location number that corresponds to any of the initial seed numbers?
 """
 
-import functools
 import threading
 import re
 import math
@@ -27,14 +26,40 @@ while True:
     except EOFError:
         break
 
-    if line:
-        lines.append(line)
+    if not line and lines[-1] == line:
+        break
+    lines.append(line)
 
+# parse the seeds
 seeds_numbers = [
     tuple(map(int, match.split(" "))) for match in re.findall(r"(\d+ \d+)+", lines[0])
 ]
 
+# convert it to start and end
+seeds_numbers = [
+    (start, start + length) for start, length in seeds_numbers
+]
 
+# helper function
+def is_overlapping(range1: tuple[int, int], range2: tuple[int, int]) -> bool:
+    """Check if two ranges are overlapping."""
+    return range1[0] <= range2[0] <= range1[1] or range2[0] <= range1[0] <= range2[1]
+
+# condense overlapping ranges of seeds
+new_seeds_numbers: list[tuple[int, int]] = [seeds_numbers[0]]
+
+for seed_start, seed_end in seeds_numbers[1:]:
+    extended_range = False
+
+    for i, (start, end) in enumerate(new_seeds_numbers):
+        if is_overlapping((seed_start, seed_end), (start, end)):
+            new_seeds_numbers[i] = (min(start, seed_start), max(end, seed_end))
+            extended_range = True
+            break
+
+    if not extended_range:
+        new_seeds_numbers.append((seed_start, seed_end))
+        
 def parse_section(section_name: str) -> list[tuple[int, int, int]]:
     """Parse a section of the input file."""
 
@@ -67,48 +92,6 @@ def map_number(number: int, map_: list[tuple[int, int, int]]) -> int:
     return number
 
 
-@functools.cache
-def get_soil(seed_number: int) -> int:
-    """Get the soil number for a seed number."""
-    return map_number(seed_number, seed_to_soil_map)
-
-
-@functools.cache
-def get_fertilizer(soil_number: int) -> int:
-    """Get the fertilizer number for a soil number."""
-    return map_number(soil_number, seed_to_fertilizer_map)
-
-
-@functools.cache
-def get_water(fertilizer_number: int) -> int:
-    """Get the water number for a fertilizer number."""
-    return map_number(fertilizer_number, fertilizer_to_water_map)
-
-
-@functools.cache
-def get_light(water_number: int) -> int:
-    """Get the light number for a water number."""
-    return map_number(water_number, water_to_light_map)
-
-
-@functools.cache
-def get_temperature(light_number: int) -> int:
-    """Get the temperature number for a light number."""
-    return map_number(light_number, light_to_temperature_map)
-
-
-@functools.cache
-def get_humidity(temperature_number: int) -> int:
-    """Get the humidity number for a temperature number."""
-    return map_number(temperature_number, temperature_to_humidity_map)
-
-
-@functools.cache
-def get_location(humidity_number: int) -> int:
-    """Get the location number for a humidity number."""
-    return map_number(humidity_number, humidity_to_location_map)
-
-
 def get_location_from_seed(seed_number: int) -> int:
     """Get the location number for a seed number."""
 
@@ -121,23 +104,47 @@ def get_location_from_seed(seed_number: int) -> int:
     return map_number(humidity, humidity_to_location_map)
 
 
-min_seed_number = math.inf
+min_location_number = math.inf
 
 
-def find_min_location_thread(seed_number: int, seed_count: int) -> None:
+def find_min_location_thread(start: int, end: int) -> None:
     """Find the min location number for a seed number."""
-    global min_seed_number
+    global min_location_number
 
-    min_seed_number = min(
-        min_seed_number,
-        *map(get_location_from_seed, range(seed_number, seed_number + seed_count)),
-    )
+    SKIP_AMOUNT = 100
+
+    # SKIP a certain amount of seeds and see if the distance is smaller or bigger than expected to then redo the search in that area. We then repeat this process until we find the min location number for the seed range.
+
+    current_number = start
+    previous_location_number = get_location_from_seed(current_number)
+
+    if previous_location_number < min_location_number:
+        min_location_number = previous_location_number
+        print(min_location_number)
+
+    for skip_i in range(start, end, SKIP_AMOUNT):
+        location_number = get_location_from_seed(skip_i)
+
+        # get the distance between the current location number
+        distance = location_number - previous_location_number
+
+        if distance != SKIP_AMOUNT:
+            for i in range(current_number, skip_i):
+                location_number = get_location_from_seed(i)
+
+                if location_number < min_location_number:
+                    min_location_number = location_number
+                    print(min_location_number)
+
+        previous_location_number = location_number
+        current_number = skip_i
 
 
-threads = []
-for seed_number, seed_count in seeds_numbers:
+threads: list[threading.Thread] = []
+for start, end in seeds_numbers:
     thread = threading.Thread(
-        target=find_min_location_thread, args=(seed_number, seed_count), daemon=True
+        target=find_min_location_thread, args=(
+            start, end), daemon=True
     )
     threads.append(thread)
     thread.start()
@@ -145,4 +152,4 @@ for seed_number, seed_count in seeds_numbers:
 for thread in threads:
     thread.join()
 
-print(min_seed_number)
+print(min_location_number)
